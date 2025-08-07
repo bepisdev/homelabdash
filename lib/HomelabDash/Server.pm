c0f8716bc9d29469c5b43a6f5bbc990aadac9413
Josh Burns <joshyburnss@gmail.com>
2025-08-07 19:46:07 +1000

    create server module

---
+++ lib/HomelabDash/Server.pm
@@ -0,0 +1,34 @@
+package HomelabDash::Server;
+use strict;
+use warnings;
+
+use FindBin;
+use lib "$FindBin::Bin/../lib";
+
+use Mojolicious::Lite -signatures;
+use HomelabDash::Logger;
+
+sub run {
+	my ($class, $config) = @_;
+
+	my $logger = HomelabDash::Logger::get();
+	my $port = $config->{server_port} || 3333;
+
+	app->defaults(config => $config);
+
+	get '/' => sub {
+		my $c = shift;
+		my $config = $c->stash('config') || app->defaults->{config};
+
+		$logger->info("Rendering dashboard page");
+
+		$c->render(
+			template => 'dashboard',
+			sections => $config->{sections} || [],
+			title    => $config->{title} || 'Homelab Dashboard'
+		);
+	};
+
+	$logger->info("Starting HomelabDash on port $port");
+	app->start;
+}
