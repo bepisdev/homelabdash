f4660de77c12370ff05603f16d57ea83885d802c
Josh Burns <joshyburnss@gmail.com>
2025-08-07 22:01:06 +1000

    use GetOpt::Long to supply config path

--- lib/HomelabDash/Config.pm
+++ lib/HomelabDash/Config.pm
@@ -16,13 +16,7 @@ our $CONFIG;
 sub load_config {
 	my ($config_path) = @_;
 
-	# FIXME: Find a better solution for allowing server to boot without a config file.
-	
-	unless (-e $config_path) {
-		die "Configuration file not found: $config_path";
-	}
-
-	my $yaml_config = LoadFile($config_path) if -e $config_path;
+	my $yaml_config = LoadFile($config_path);
 
 	# Merge ENV variables into config
 	$yaml_config->{server_port}	= $ENV{'PORT'} if defined $ENV{'PORT'} || 3333;
