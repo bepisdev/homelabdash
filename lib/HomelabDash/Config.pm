bedf4e6610b3d8fe037d680838d5460c90ed8542
Josh Burns <joshyburnss@gmail.com>
2025-08-07 20:17:54 +1000

    allow config module to load without config file (for testing)

--- lib/HomelabDash/Config.pm
+++ lib/HomelabDash/Config.pm
@@ -9,11 +9,11 @@ our $CONFIG;
 sub load_config {
 	my $config_path = '/config/dashboard.yml';
 
-	unless (-e $config_path) {
-		die "Configuration file not found: $config_path";
-	}
+	# unless (-e $config_path) {
+	# 	die "Configuration file not found: $config_path";
+	# }
 
-	my $yaml_config = LoadFile($config_path);
+	my $yaml_config = LoadFile($config_path) if -e $config_path;
 
 	# Merge ENV variables into config
 	$yaml_config->{server_port}	= $ENV{'PORT'} if defined $ENV{'PORT'};
