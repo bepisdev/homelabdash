39d321dbe3f751db37f3ebbb122245cebfa56fd7
Josh Burns <joshyburnss@gmail.com>
2025-08-07 20:04:57 +1000

    fix typos

--- lib/HomelabDash/Config.pm
+++ lib/HomelabDash/Config.pm
@@ -9,21 +9,21 @@ our $CONFIG;
 sub load_config {
 	my $config_path = '/config/dashboard.yml';
 
-	unless (-e $config_path) {}
+	unless (-e $config_path) {
 		die "Configuration file not found: $config_path";
 	}
 
 	my $yaml_config = LoadFile($config_path);
 
 	# Merge ENV variables into config
-	$yaml_config->{server_port}	= $ENV{'PORT'} if defined $ENV{'PORT'}
-	$yaml_config->{server_log_level} = $ENV{'LOG_LEVEL'} if defined $ENV{'LOG_LEVEL'}
+	$yaml_config->{server_port}	= $ENV{'PORT'} if defined $ENV{'PORT'};
+	$yaml_config->{server_log_level} = $ENV{'LOG_LEVEL'} if defined $ENV{'LOG_LEVEL'};
 
 	$CONFIG = $yaml_config;
 	return $CONFIG;
 }
 
-sub get {}
+sub get {
 	my $key = shift;
 	return $CONFIG->{$key};
 }
