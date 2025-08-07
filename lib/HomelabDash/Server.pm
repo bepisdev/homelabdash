8f0ef1ae986922e9b77b1dddd0c81abb246d89df
Josh Burns <joshyburnss@gmail.com>
2025-08-07 22:24:04 +1000

    strip off template renderer to ensure load path works

--- lib/HomelabDash/Server.pm
+++ lib/HomelabDash/Server.pm
@@ -14,19 +14,14 @@ sub run {
 	my $logger = HomelabDash::Logger::get();
 	my $port = $config->{server_port} || 3333;
 
-	app->defaults(config => $config);
+	app->defaults->{config} = $config;
 	app->renderer->paths(["$FindBin::Bin/../templates"]); 
 
 	get '/' => sub {
 		my $c = shift;
-		my $config = $c->stash('config') || app->defaults->{config};
-
 		$logger->info("Rendering dashboard page");
-
 		$c->render(
 			template => 'dashboard',
-			sections => $config->{sections} || [],
-			page_title => $config->{page_title} || 'HomelabDash'
 		);
 	};
 
